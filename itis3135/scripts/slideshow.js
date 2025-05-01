145f003adbd9bf10f87945dd65c72da3f40909f8
“Justin” <jlee356@uncc.edugit config --global user.email jlee356@uncc.edu>
2025-04-30 22:09:02 -0400

    update slideshow

---
+++ itis3135/scripts/slideshow.js
@@ -0,0 +1,32 @@
+let slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("demo");
+  const captionText = document.getElementById("caption");
+
+  if (n > slides.length) slideIndex = 1;
+  if (n < 1) slideIndex = slides.length;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add("active");
+  captionText.innerText = dots[slideIndex - 1].alt;
+}
+
+showSlides(slideIndex);
