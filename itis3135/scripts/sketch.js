6242950647710466a0b6d616fd47b6d4737240fe
“Justin” <jlee356@uncc.edugit config --global user.email jlee356@uncc.edu>
2025-04-30 22:41:00 -0400

    update drawing.html

---
+++ itis3135/scripts/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(600, 400);
+    background(220);
+  }
+  
+  function draw() {
+    fill(100, 150, 200);
+    noStroke();
+    ellipse(mouseX, mouseY, 50, 50);
+  }
+  
